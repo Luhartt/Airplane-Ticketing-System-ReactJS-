66b157157b1b33aafaa3abd1708482a571daffae
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2024-12-10 16:04:39 +0800

    Update
    *Added dependencies
    *Finished Sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,15 +1,37 @@
+import React, { useState } from "react";
+import "./sidebar.css";
+import DashboardIcon from "../Dependencies/iconDashboard.png";
+import BookFlights from "../Dependencies/iconFlightBooking.png";
+import ViewBookings from "../Dependencies/iconViewBookings.png";
+import Account from "../Dependencies/iconProfile.png";
 
-function sidebar() {
+function Sidebar() {
+
+  const[active, setActive] = useState(null);
+
+    const handleClick = (index) => {
+      setActive(index)
+    }
+
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  )
+    <aside>
+      {[
+        {icon: DashboardIcon, label: "Flights"},
+        {icon: BookFlights, label: "Book Flights"},
+        {icon: ViewBookings, label: "View Bookings"},
+        {icon: Account, label: "Profile"},
+      ].map((item, index) => (
+        <div
+          key={index}
+          className = {`sidebar-item-${active === index ? "active" : ""}`}
+          onClick = {() => handleClick(index)}
+        >
+        <img src={item.icon} alt = 'icon'/>
+        {item.label}
+        </div>
+      ))}
+    </aside>
+
+  );
 }
-
-export default sidebar;
-
-
+export default Sidebar;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Sidebar from './components/sidebar';
+import Sidebar from './components/sidebar.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
