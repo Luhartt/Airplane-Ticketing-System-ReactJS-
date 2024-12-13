6ed464140512530e39ec83d75874a93639a3e2e4
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2024-12-13 21:22:31 +0800

    Feat: Added ContentPanel to App.jsx
    *Started development of flights dashboard
    * Added ContentPanel to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,29 @@
-import SidebarHeader from './sidebar-header'
-import FlightView from './FlightsView'
-import "./App.css"
+import SidebarHeader from "./sidebar-header";
+import FlightsDashboard from "./FlightsDashboard";
+import "./App.css";
 
-export default function App(){
+export default function App() {
+  const sampleFlight = {
+    AirplaneNumber: "AB123",
+    DepartureDate: "December 09, 2024",
+    FlightData: {
+      DepartureLocation: "ORD",
+      DepartureTime: "19:00",
+      ArrivalLocation: "JFK",
+      ArrivalTime: "20:00",
+    },
+  };
 
-      const sampleFlight = {
-        AirplaneNumber: "AB123",
-        DepartureDate: "December 09, 2024",
-        FlightData: {
-          DepartureLocation: "ORD",
-          DepartureTime: "19:00",
-          ArrivalLocation: "JFK",
-          ArrivalTime: "20:00",
-        }
-      }
-
-    return(
-        <>
-            <div className='Main'>
-                <SidebarHeader className="SidebarHeader"></SidebarHeader>
-                <section className='MainSection'>
-                    <FlightView FlightData={sampleFlight}></FlightView>
-                    <FlightView FlightData={sampleFlight}></FlightView>
-                    <FlightView FlightData={sampleFlight}></FlightView>
-                    <FlightView FlightData={sampleFlight}></FlightView>
-
-                </section>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="Main">
+        <SidebarHeader className="SidebarHeader"></SidebarHeader>
+        <section className="MainSection">
+          <div className="ContentPanel">
+            <FlightsDashboard></FlightsDashboard>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
