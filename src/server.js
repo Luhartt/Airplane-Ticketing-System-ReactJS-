54e16c37b61fc76e962a324214c3a9a31c0d679f
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2024-12-15 12:15:20 +0800

    started backend development
    *data for flights dashboard fetched

---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "airplaneticketingsystem2024",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+app.get("/FlightsDashboard", (req, res) => {
+  const query = `
+SELECT Flights.DepartureDate, Flights.ArrivalDate, Flights.AirplaneNumber, Flights.FlightID,
+ArrivalAirport.AirportCode AS ArrivalAirportCode, DepartureAirport.AirportCode AS DepartureAirportCode
+FROM flights
+JOIN Airport AS DepartureAirport ON Flights.DepartureAirportID = DepartureAirport.AirportID
+JOIN Airport AS ArrivalAirport ON Flights.ArrivalAirportID = ArrivalAirport.AirportID
+WHERE flights.DepartureDate >= NOW()
+ORDER BY Flights.DepartureDate ASC;
+    `;
+    db.query(query, (err, results) => {
+        if(err) res.status(500).json({error:err});
+        res.json(results)
+        console.log(res);
+    })
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
