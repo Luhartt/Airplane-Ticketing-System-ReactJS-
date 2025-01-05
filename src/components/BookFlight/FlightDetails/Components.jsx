80da4b63360c4c700d23cd2c7c53d02e31071340
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2025-01-05 15:04:28 +0800

    reusable components for flight details

---
+++ src/components/BookFlight/FlightDetails/Components.jsx
@@ -0,0 +1,146 @@
+import ComboBox from "./ComboBox";
+import Button from "../../Button";
+import FlightFly from "../../../Dependencies/flight_takeoff.png";
+import FlightLand from "../../../Dependencies/flight_takeoff.png";
+import Calendar from "../../../Dependencies/calendar_today.png";
+import SuitCase from "../../../Dependencies/work.png";
+import Adult from "../../../Dependencies/person.png";
+import Infant from "../../../Dependencies/face.png";
+import Children from "../../../Dependencies/child_care.png";
+
+const Locations = ({ data, setData, options }) => (
+  <fieldset className="Locations">
+    <div className="LocationsContainer">
+      <p> From </p>
+      <p>Ninoy Aquino International Airport (MNL)</p>
+      <img src={FlightFly} alt="Takeoff" />
+      <ComboBox
+        options={options.DepartureLocations || []}
+        setData={setData}
+        data={data}
+        keyName="Departure Location"
+      ></ComboBox>
+    </div>
+    <hr />
+    <div className="LocationsContainer">
+      <p> To </p>
+      <p>Ninoy Aquino International Airport (MNL)</p>
+      <img src={FlightLand} alt="Land" />
+      <ComboBox
+        options={options.ArrivalLocations || []}
+        setData={setData}
+        data={data}
+        keyName="Arrival Location"
+      ></ComboBox>
+    </div>
+  </fieldset>
+);
+
+const DatesOneWay = ({ data, setData, options }) => (
+  <fieldset className="DepartureDate">
+    <p> Departure Date</p>
+    <img src={Calendar} alt="Date" />
+    <ComboBox
+      options={options.DepartureDates || []}
+      setData={setData}
+      data={data}
+      keyName="Departure Date"
+    ></ComboBox>
+  </fieldset>
+);
+
+const DatesRoundTrip = ({ data, setData, options }) => (
+  <fieldset className="DepartureDate">
+    <div className="DateContainer">
+      <p> Departure Date</p>
+      <img src={Calendar} alt="Date" />
+      <ComboBox
+        options={options.DepartureDates || []}
+        setData={setData}
+        data={data}
+        keyName="Departure Date"
+      ></ComboBox>
+    </div>
+    <div className="DateContainer">
+      <p> Return Date</p>
+      <img src={Calendar} alt="Date" />
+      <ComboBox
+        options={options.DepartureDates || []}
+        setData={setData}
+        data={data}
+        keyName="Departure Date"
+      ></ComboBox>
+    </div>
+  </fieldset>
+);
+
+const ClassSeat = ({ data, setData }) => (
+  <fieldset className="ClassSeat">
+    <p> Class Seat</p>
+    <img src={SuitCase} alt="Seat Class" />
+    <ComboBox
+      options={[
+        "Economy",
+        "Business Economy",
+        "Premium",
+        "Premium Economy",
+        "First Class",
+      ]}
+      setData={setData}
+      data={data}
+      keyName="Class Seat"
+    ></ComboBox>
+  </fieldset>
+);
+
+function PassengerCount({ data, setData }) {
+  const passengerInputs = [
+    { icon: Adult, label: "Adult", id: "adultCount" },
+    { icon: Children, label: "Children (2-11 Years Old)", id: "childrenCount" },
+    { icon: Infant, label: "Infant", id: "infantCount" },
+  ];
+
+  const handleTextInput = (e, keyName) => {
+    e.preventDefault();
+    const value = parseInt(e.target.value, 10) || 0;
+
+    if (isNaN(value)) {
+      alert("Input a number");
+      setData({ ...data, [keyName]: keyName === "Adult" ? 1 : 0 });
+    } else {
+      setData({ ...data, [keyName]: value });
+    }
+  };
+
+  return (
+    <fieldset className="PassengerCount">
+      {passengerInputs.map((item, index) => (
+        <div key={`${item.label} ${index} container`}>
+          <p key={`${item.label} ${index} label`}>{item.label}</p>
+          <img
+            key={`${item.label} ${index} image`}
+            src={item.icon}
+            alt={item.label}
+          />
+          <input
+            onChange={(event) => handleTextInput(event, item.label)}
+            key={`${item.label} ${index} input`}
+            type="number"
+            min={item.label === "Adult" ? 1 : 0}
+            max={99}
+            id={item.label}
+            value={data[item.label] || (item.label === "Adult" ? 1 : 0)}
+          />
+        </div>
+      ))}
+    </fieldset>
+  );
+}
+
+export const Components = {
+  Locations,
+  DatesOneWay,
+  DatesRoundTrip,
+  ClassSeat,
+  PassengerCount,
+};
--- src/components/BookFlight/FlightDetails/OneWay.jsx
+++ src/components/BookFlight/FlightDetails/OneWay.jsx
@@ -8,6 +8,7 @@ import SuitCase from "../../../Dependencies/work.png";
 import Adult from "../../../Dependencies/person.png";
 import Infant from "../../../Dependencies/face.png";
 import Children from "../../../Dependencies/child_care.png";
+import { Components } from "./Components";
 
 export default function OneWay({ data, setData }) {
   const Flights = [
@@ -22,22 +23,30 @@ export default function OneWay({ data, setData }) {
     "Taguig",
   ];
 
-  const passengerInputs = [
-    { icon: Adult, label: "Adult", id: "adultCount" },
-    { icon: Children, label: "Children (2-11 Years Old)", id: "childrenCount" },
-    { icon: Infant, label: "Infant", id: "infantCount" },
-  ];
-
-  const handleTextInput = (e, keyName) => {
-    e.preventDefault();
-    const value = parseInt(e.target.value, 10) || 0;
-
-    if (isNaN(value)) {
-      alert("Input a number");
-      setData({ ...data, [keyName]: keyName === "Adult" ? 1 : 0 });
-    } else {
-      setData({ ...data, [keyName]: value });
-    }
+  const options = {
+    DepartureLocations: [
+      "Manila",
+      "Cebu",
+      "Taguig",
+      "Manila",
+      "Cebu",
+      "Taguig",
+      "Manila",
+      "Cebu",
+      "Taguig",
+    ],
+    ArrivalLocations: [
+      "Manila",
+      "Cebu",
+      "Taguig",
+      "Manila",
+      "Cebu",
+      "Taguig",
+      "Manila",
+      "Cebu",
+      "Taguig",
+    ],
+    DepartureDates: ["1", "2", "3", "4"],
   };
 
   const test = () => {
@@ -48,78 +57,29 @@ export default function OneWay({ data, setData }) {
   return (
     <>
       <form action="">
-        <fieldset className="Locations">
-          <div className="LocationsContainer">
-            <p> From </p>
-            <p>Ninoy Aquino International Airport (MNL)</p>
-            <img src={FlightFly} alt="Takeoff" />
-            <ComboBox
-              options={Flights}
-              setData={setData}
-              data={data}
-              keyName="Departure Location"
-            ></ComboBox>
-          </div>
-          <hr />
-          <div className="LocationsContainer">
-            <p> To </p>
-            <p>Ninoy Aquino International Airport (MNL)</p>
-            <img src={FlightLand} alt="Land" />
-            <ComboBox
-              options={Flights}
-              setData={setData}
-              data={data}
-              keyName="Arrival Location"
-            ></ComboBox>
-          </div>
-        </fieldset>
-        <fieldset className="DepartureDate">
-          <p> Departure Date</p>
-          <img src={Calendar} alt="Date" />
-          <ComboBox
-            options={Flights}
-            setData={setData}
+        {
+          <Components.Locations
+            options={options}
             data={data}
-            keyName="Departure Date"
-          ></ComboBox>
-        </fieldset>
-        <fieldset className="ClassSeat">
-          <p> Class Seat</p>
-          <img src={SuitCase} alt="Seat Class" />
-          <ComboBox
-            options={[
-              "Economy",
-              "Business Economy",
-              "Premium",
-              "Premium Economy",
-              "First Class",
-            ]}
             setData={setData}
+          ></Components.Locations>
+        }
+        {
+          <Components.DatesOneWay
+            options={options}
             data={data}
-            keyName="Class Seat"
-          ></ComboBox>
-        </fieldset>
-        <fieldset className="PassengerCount">
-          {passengerInputs.map((item, index) => (
-            <div key={`${item.label} ${index} container`}>
-              <p key={`${item.label} ${index} label`}>{item.label}</p>
-              <img
-                key={`${item.label} ${index} image`}
-                src={item.icon}
-                alt={item.label}
-              />
-              <input
-                onChange={(event) => handleTextInput(event, item.label)}
-                key={`${item.label} ${index} input`}
-                type="number"
-                min={item.label === "Adult" ? 1 : 0}
-                max={99}
-                id={item.label}
-                value={data[item.label] || (item.label === "Adult" ? 1 : 0)}
-              />
-            </div>
-          ))}
-        </fieldset>
+            setData={setData}
+          ></Components.DatesOneWay>
+        }
+
+        {
+          <Components.ClassSeat
+            data={data}
+            setData={setData}
+          ></Components.ClassSeat>
+        }
+        {<Components.PassengerCount data={data} setData={setData}></Components.PassengerCount>}
+
         <Button
           text={"SEARCH FLIGHT"}
           handleClick={test}
