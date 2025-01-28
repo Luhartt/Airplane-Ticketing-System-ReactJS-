223e283a07403b60c526842e678219dbb2fdebf3
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2025-01-28 15:53:24 +0800

    feat: addons can be selected

--- src/components/BookFlight/Addons.jsx
+++ src/components/BookFlight/Addons.jsx
@@ -1,9 +1,54 @@
-import { useData } from "../DataSetter";
+import { Routes, Route } from "react-router";
+import { useData } from "./DataSetter";
+import "./Addons.css";
+import { useState } from "react";
+import FoodImage from "../../Dependencies/Addon-Food.png";
+import BaggageImage from "../../Dependencies/Addon-Baggage.png";
+import TransportImage from "../../Dependencies/Addon-Transport.png";
 
 const Addon = () => {
+  const {data, setData} = useData()
+  const [active, setActive] = useState(data.SelectedAddonIndex || -1);
 
-}
+  const addons = [
+    { image: FoodImage, Text: "FOOD" },
+    { image: BaggageImage, Text: "BAGGAGE" },
+    { image: TransportImage, Text: "TRANSPORT" },
+  ];
 
-export default function Addons(){
-    
+  const SelectAddon = ({ index, addon }) => {
+    setActive(index);
+    const selectedAddon = addon.toUpper();
+    setData((prevdata) => ({
+        ...prevdata, ["SelectedAddonIndex"]: index
+    }))
+    setData((prevdata) => ({
+        ...prevdata, ["SelectedAddon"]: selectedAddon
+    }))
+  };
+
+  return (
+    <section className="AddonsContainer">
+      {[
+        addons.map((item, index) => (
+          <div
+            className={`addon ${active === index && "Active"}`}
+            key={`addon-${index}`}
+            onClick={() => SelectAddon(index, item.Text)}
+          >
+            <img src={item.image} alt="" key={`addon-image-${index}`} />
+            <p key={`addon-text-${index}`}>{item.Text}</p>
+          </div>
+        )),
+      ]}
+    </section>
+  );
+};
+
+export default function Addons() {
+  return (
+    <Routes>
+      <Route path="addons" element={<Addon></Addon>}></Route>
+    </Routes>
+  );
 }
--- src/components/BookFlight/BookFlight.jsx
+++ src/components/BookFlight/BookFlight.jsx
@@ -2,6 +2,7 @@ import "./BookFlight.css";
 import FlightDetails from "./FlightDetails/FlightDetails";
 import AvailableFlights from "./AvailableFlights";
 import PassengerDetails from "./PassengerDetails";
+import Addons from "./Addons";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { DataProvider } from "./DataSetter";
@@ -20,9 +21,10 @@ export default function BookFlights() {
   return (
     <section className="bookFlights">
       <DataProvider>
-        <FlightDetails setType={setType}></FlightDetails>
+        {/* <FlightDetails setType={setType}></FlightDetails> */}
         <AvailableFlights></AvailableFlights>
         <PassengerDetails></PassengerDetails>
+        <Addons></Addons>
       </DataProvider>
     </section>
   );
